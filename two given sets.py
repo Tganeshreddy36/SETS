e273642a295f671f91da6a7830b96896e5571b18
Tganeshreddy36 <ganeshreddyt2@gmail.com>
2024-10-14 22:44:32 -0700

    Update two given sets.py

--- two given sets.py
+++ two given sets.py
@@ -6,3 +6,15 @@ Sample Input:
 Sample Output:
 1 3
 """
+# Read the first set of values
+set1 = set(map(int, input("Enter the first set of values: ").split()))
+
+# Read the second set of values
+set2 = set(map(int, input("Enter the second set of values: ").split()))
+
+# Calculate the symmetric difference between the two sets
+different_values = set1.symmetric_difference(set2)
+
+# Print the different values space-separated
+print(" ".join(map(str, sorted(different_values))))
+"""
