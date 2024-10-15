700bf08cdfd6aaed246db4872d735b22c79b9f6c
Tganeshreddy36 <ganeshreddyt2@gmail.com>
2024-10-14 22:42:12 -0700

    Update given set in another set.py

--- given set in another set.py
+++ given set in another set.py
@@ -6,3 +6,15 @@ Sample Input:
 Sample Output:
 1 2 3 4 5
 """
+# Read the first set of values from input
+set1 = set(map(int, input("Enter the first set of values: ").split()))
+
+# Read the second set of values from input
+set2 = set(map(int, input("Enter the second set of values: ").split()))
+
+# Update the first set with the second set
+set1.update(set2)
+
+# Print the updated set values space-separated
+print(" ".join(map(str, sorted(set1))))
+"""
