484547242793a594aedb20c28d07252bacabe37c
Tganeshreddy36 <ganeshreddyt2@gmail.com>
2024-10-14 22:45:12 -0700

    Update similar both sets.py

--- similar both sets.py
+++ similar both sets.py
@@ -6,3 +6,15 @@ Sample Input:
 Sample Output:
 2 4 
 """
+# Read the first set of values
+set1 = set(map(int, input("Enter the first set of values: ").split()))
+
+# Read the second set of values
+set2 = set(map(int, input("Enter the second set of values: ").split()))
+
+# Calculate the intersection between the two sets
+common_values = set1.intersection(set2)
+
+# Print the common values space-separated
+print(" ".join(map(str, sorted(common_values))))
+"""
