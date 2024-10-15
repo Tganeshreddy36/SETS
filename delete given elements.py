a0d5114a4831a5e21f7385ba86073b17623c7701
Tganeshreddy36 <ganeshreddyt2@gmail.com>
2024-10-14 22:47:30 -0700

    Update delete given elements.py

--- delete given elements.py
+++ delete given elements.py
@@ -6,3 +6,17 @@ Sample Input:
 Sample Output:
 1 3 4 
 """
+# Read the set values from input
+set_values = set(map(int, input("Enter the set values separated by space: ").split()))
+
+# Read the element to be deleted
+element_to_delete = int(input("Enter the element to delete: "))
+
+# Check if the element is in the set and delete it if present
+if element_to_delete in set_values:
+    set_values.remove(element_to_delete)
+    # Print the updated set values space-separated
+    print(" ".join(map(str, sorted(set_values))))
+else:
+    print("Given value is not present in the set list.")
+"""
