e36b8642ced942537bf1800adec3f3aaa03519f5
Tganeshreddy36 <ganeshreddyt2@gmail.com>
2024-10-14 22:24:00 -0700

    single line.py

---
+++ single line.py
@@ -0,0 +1,13 @@
+"""
+Write a program to get the set values in a single line separated by space (including duplicate values) and print the number of elements in the given set.
+Sample Input:
+1 2 3 4 5 1 2 3
+Sample Output:
+5
+7) Write a program to find the maximum and minimum value of given set values.
+Sample Input:
+1 2 3 4 5
+Sample Output:
+Maximum: 5
+Minimum: 1
+"""
