6a3a1f4a0c662d1b744a473ae6fc779d0cf3e61f
Tganeshreddy36 <ganeshreddyt2@gmail.com>
2024-10-14 22:46:24 -0700

    Update input in single.py

--- input in single.py
+++ input in single.py
@@ -5,3 +5,15 @@ Sample Input:
 Sample Output:
 1 2 3 4 5
 """
+# Read the values in a single line
+values = input("Enter the values separated by space: ").split()
+
+# Create a set from the values to remove duplicates
+unique_values = set(map(int, values))
+
+# Convert the set to a sorted list
+sorted_values = sorted(unique_values)
+
+# Print the sorted values space-separated
+print(" ".join(map(str, sorted_values)))
+"""
